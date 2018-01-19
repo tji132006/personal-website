f6773b0126dbe061b4256ba66047f97c6af1c4a8
unknown <tji132006@gmail.com>
2018-01-18 17:19:23 -0800

    add background image

---
+++ photo/data.js
@@ -0,0 +1,29 @@
+var data = [];
+
+var dataStr = 
+'1、 test<br>\
+<br>\
+导演大量发生的连发家里地方<br>\
+<br>\
+<br>\
+2、 tree<br>\
+<br>\
+发家里地方<br>\
+<br>\
+<br>\
+3、 tree2<br>\
+<br>\
+大大方<br>\
+';
+
+var d = dataStr.split('<br><br><br>')
+for(var i = 0; i < d.length; i++){
+	var c = d[i].split('<br><br>');
+	data.push({
+		img: c[0].replace('、', ' ') + '.jpg',
+		caption: c[0].split('、')[1],
+		desc: c[1]
+	});
+	console.log(c[0].replace('、', ' ')+'.jpg');
+
+}
