e2ab1ca270ad3ac690dca5e6aebdf4605ab79163
unknown <tji132006@gmail.com>
2018-02-02 16:08:27 -0800

    starting add back style

--- photo/data.js
+++ photo/data.js
@@ -1,22 +1,22 @@
 var data = [];
 
 var dataStr = 
-`1、test<br>\
+`1、20-Stamp<br>\
 <br>\
 导演大量发生的连发家里地方<br>\
 <br>\
 <br>\
-2、tree<br>\
+2、shadow<br>\
 <br>\
 发家里地方<br>\
 <br>\
 <br>\
-3、t<br>\
+3、post<br>\
 <br>\
 发家里地方<br>\
 <br>\
 <br>\
-4、ee<br>\
+4、badge<br>\
 <br>\
 大大方<br>\
 `;
@@ -25,7 +25,7 @@ var d = dataStr.split('<br><br><br>')
 for(var i = 0; i < d.length; i++){
 	var c = d[i].split('<br><br>');
 	data.push({
-		img: c[0].replace('、', ' ') + '.jpg',
+		img: c[0].replace('、', ' ') + '.png',
 		caption: c[0].split('、')[1],
 		desc: c[1]
 	});
